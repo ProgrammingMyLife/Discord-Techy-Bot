770cf22260b9851a53e913d76bc81b46993feba4
Sai Parjanya <saiparjanyam@gmail.com>
2021-05-27 11:44:23 +0530

    'awesome_stuff'

--- techy_bot.py
+++ techy_bot.py
@@ -1,24 +1,21 @@
 
 import discord
 import os
-from sys import stdout
+import datetime
 
 client = discord.Client()
+
+
+dt = datetime.datetime.now()
+hour = dt.hour
+
 
 @client.event
 async def on_ready():
      print(f'Bot is online')
 
-def part_of_day(hour):
-    return (
-        "morning" if 5 <= hour <= 11
-        else
-        "afternoon" if 12 <= hour <= 17
-        else
-        "evening" if 18 <= hour <= 22
-        else
-        "night"
-    )
+
+part_of_day(hour)
 
 
 @client.event
@@ -34,35 +31,36 @@ async def on_message(message):
 1.'/hello' -> For telling "hi" to the bot
 2.'/help' -> To know the commands
 3.'/play' -> Function to be added
-4.'/gm'-> Function to be added
-5.'/gn' -> Function to be added
-6.'/ga '-> Function to be added
-More commands to be added..
+4.'/gm'-> To say Good Morning.
+5.'/gn' -> To say Good night
+6.'/ga '-> To say Good Afternoon
+
+Sorry for inconvenience! More functions will be added soon
+
      ''' + message.author.mention)
 
 
-#ok ok
 
-# all gm's
+
     if message.content.startswith('/gm'):
-        if part_of_day == 'morning':
+        if hour < 12:
             await message.channel.send('Good Morning' + message.author.mention + '.' + 'Have nice day ahead')
         else:
             await message.channel.send("Ah.. It's not morning. " + message.author.mention + "I am not a fool (just informing you..) I don't wanna be rude tho")
 
     if message.content.startswith('/ga'):
-        if part_of_day == 'afternoon':
+        if hour > 12 and hour < 5:
             await message.channel.send('Good Afternoon' + message.author.mention + '.' + 'Have lunch!')
         else:
             await message.channel.send("Ah.. It's not noon. " + message.author.mention + "I am not a fool (just informing you..)")
 
     if message.content.startswith('/gn'):
-        if part_of_day == 'night':
+        if hour > 19 and hour < 5:
             await message.channel.send('Good Night' + message.author.mention + '.' + 'Sweet Dreams..')
         else:
             await message.channel.send("Ah.. It's not night." + message.author.mention + "I am not a fool (just informing you..)")
-
-
+    if message.content.startswith('/play'):
+        await message.channel.send('Function yet to be added...')
 
 #run
 client.run(os.environ['Token'])
